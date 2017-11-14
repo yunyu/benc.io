c61f807a3779964d4d46368bc7f7d3e7a8ed7aac
Benjamin Cooper <bencooper222@gmail.com>
2017-11-14 16:18:11 -0600

    Added new articles

--- resources/js/randomArticle.js
+++ resources/js/randomArticle.js
@@ -17,7 +17,9 @@ const articleLinks = ["http://waitbutwhy.com/2014/05/fermi-paradox.html",
     "https://www.theatlantic.com/technology/archive/2015/07/the-secret-startup-saved-healthcare-gov-the-worst-website-in-america/397784/",
     "https://www.theatlantic.com/education/archive/2017/11/the-surprising-revolt-at-reed/544682/?utm_source=vxfb",
     "http://www.motherjones.com/politics/2016/06/cca-private-prisons-corrections-corporation-inmates-investigation-bauer/",
-    "https://www.nytimes.com/interactive/2017/01/18/upshot/some-colleges-have-more-students-from-the-top-1-percent-than-the-bottom-60.html"
+    "https://www.nytimes.com/interactive/2017/01/18/upshot/some-colleges-have-more-students-from-the-top-1-percent-than-the-bottom-60.html",
+    "https://www.politico.com/interactives/2017/top-college-rankings-list-2017-us-news-investigation/",
+    "https://www.stilldrinking.org/programming-sucks"
 ]; // who needs config variables?
 
 let articleLinksTemp = articleLinks;
